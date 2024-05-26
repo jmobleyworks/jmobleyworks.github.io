ab2462f75b848d4cb87bb3101260e9480083f237
jmobleyworks <77175845+jmobleyworks@users.noreply.github.com>
2024-05-26 16:19:07 -0400

    Update world.js

--- projects/spriteExtractor/world.js
+++ projects/spriteExtractor/world.js
@@ -286,6 +286,7 @@ const initialWorldSeed = {
 };
 
 function createWorldSeedForm(elementId) {
+  console.log("elementId="+elementId);
   const formContainer = document.createElement('div'); // Declare formContainer here
   const form = document.createElement('form');
   form.setAttribute('id', 'world-seed-form');

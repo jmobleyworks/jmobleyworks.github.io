5b616fc4b538c0e452d6cc8a8f91e53876b73c6b
jmobleyworks <77175845+jmobleyworks@users.noreply.github.com>
2024-05-27 17:52:58 -0400

    gg

--- projects/gameSeedWorldBuilderControl/gameDesignChoices.js
+++ projects/gameSeedWorldBuilderControl/gameDesignChoices.js
@@ -1,16 +1,14 @@
-{
-    "gameDesignChoices": {
-      "timePeriod": ["Ancient", "Medieval", "Renaissance"],
-      "theme": ["Fantasy", "Sci-Fi", "Horror"],
-      "atmosphere": ["Bright", "Dark", "Mysterious"],
-      "environment": ["Forest", "Desert", "Mountain"],
-      "location": ["Village", "Castle", "Dungeon"],
-      "landmarks": ["Statues", "Fountains", "Ruins"],
-      "inhabitants": ["Humans", "Elves", "Orcs"],
-      "conflict": ["War", "Plague", "Invasion"],
-      "elementalInfluence": ["Fire", "Water", "Earth"],
-      "technologyLevel": ["Primitive", "Advanced", "Steampunk"],
-      "culturalInfluences": ["Norse", "Egyptian", "Japanese"],
-      "narrativeFocus": ["Hero's Journey", "Revenge", "Mystery"]
-    }
-  }
+export const gameDesignChoices = {
+  timePeriod: ["Ancient", "Medieval", "Renaissance"],
+  theme: ["Fantasy", "Sci-Fi", "Horror"],
+  atmosphere: ["Bright", "Dark", "Mysterious"],
+  environment: ["Forest", "Desert", "Mountain"],
+  location: ["Village", "Castle", "Dungeon"],
+  landmarks: ["Statues", "Fountains", "Ruins"],
+  inhabitants: ["Humans", "Elves", "Orcs"],
+  conflict: ["War", "Plague", "Invasion"],
+  elementalInfluence: ["Fire", "Water", "Earth"],
+  technologyLevel: ["Primitive", "Advanced", "Steampunk"],
+  culturalInfluences: ["Norse", "Egyptian", "Japanese"],
+  narrativeFocus: ["Hero's Journey", "Revenge", "Mystery"]
+};

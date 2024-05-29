9db7252a6d931efadd37e77bf96d6782ba18dcc6
jmobleyworks <77175845+jmobleyworks@users.noreply.github.com>
2024-05-29 15:09:53 -0400

    gg

---
+++ ToDo.js
@@ -0,0 +1,5 @@
+export const ToDo = {
+    "2dActionRpg": ["Remove the map stretching thing and test that things work. Write a function to export map to image to upload to ai art gen to turn white traversable and turn black non traversalbe pixel art"],
+    "WorldSeed": ["Make it so the prompt udates whenever a control is selected or the form is cleared, add copy and donate buttons, turn randomize and clear button into glyphs and put all 4 at top right of World Builder control"],
+    "index.html": ["Make logo visible and things appear nicely"]
+}  

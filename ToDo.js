cd96626ea6258a17b67e72ef81ddf424bb830b1a
jmobleyworks <77175845+jmobleyworks@users.noreply.github.com>
2024-05-29 15:40:03 -0400

    gg

--- ToDo.js
+++ ToDo.js
@@ -1,5 +1,13 @@
 export const ToDo = {
     "2dActionRpg": ["Remove the map stretching thing and test that things work. Write a function to export map to image to upload to ai art gen to turn white traversable and turn black non traversalbe pixel art"],
     "WorldSeed": ["Make it so the prompt udates whenever a control is selected or the form is cleared, add copy and donate buttons, turn randomize and clear button into glyphs and put all 4 at top right of World Builder control"],
-    "index.html": ["Make logo visible and things appear nicely"]
+    "index.html": ["Make logo visible and things appear nicely"],
+    "WorldBender" : ["Make the structure appear on load and provide an explanation, but it in the style of the Mobleysoft Brand"],
+    "SpriteDetector" : ["Put it in style of Mobleysoft Brand, add spriteSheets gened so far as buttons and let them be clicked to switch what spritesheet has its sprites extracted"]
+    "ChatGPT TAB" deploy it:
+    "That wrap sphere guy, email him
+    "chatGpT you have 10dalle credits on jmobley@mobleysoft acct
+    "chatGpT you subscribe to 4o on johnmobley99 acc"
+    "chatGpt TAB" switch to johnmobley 99 acc and add last Convo, to do list for text2game, to this todo list.
+    
 }  

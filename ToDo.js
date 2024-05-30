73609ec44a4e3a55d5820b705bdb71e0c35d1fc9
jmobleyworks <77175845+jmobleyworks@users.noreply.github.com>
2024-05-30 15:37:36 -0400

    gg

--- ToDo.js
+++ ToDo.js
@@ -1,40 +1,52 @@
 export const ToDo = {
     "2dActionRpg": [
         "Remove the map stretching thing and test that things work.",
-        "Write a function to export map to image to upload to AI art gen to turn white traversable and turn black non-traversable pixel art."
+        "Write a function to export the map to an image to upload to AI art generator to turn white traversable and black non-traversable pixel art."
     ],
     "WorldSeed": [
         "Make it so the prompt updates whenever a control is selected or the form is cleared.",
         "Add copy and donate buttons.",
         "Turn randomize and clear buttons into glyphs and put all four at the top right of World Builder control."
     ],
-    "index.html": [
-        "Make things appear nicely."
-    ],
     "WorldBender": [
         "Make the structure appear on load and provide an explanation.",
-        "But it in the style of the Mobleysoft Brand."
+        "Put it in the style of the Mobleysoft Brand.",
+        "Come up with a name for my tool whose research paper will be something like 'A sufficiently advanced game world model as reality' that expresses how the research into perfecting a gameWorld model can be extrapolated to designing a powerful World Model, an AI that demonstrates an understanding of the world."
     ],
     "SpriteDetector": [
-        "Put it in style of Mobleysoft Brand.",
+        "Put it in the style of the Mobleysoft Brand.",
         "Add sprite sheets generated so far as buttons and let them be clicked to switch what sprite sheet has its sprites extracted."
     ],
     "chatGpT": [
-        "You have 10 DALL-E credits on jmobley@mobleysoft account.",
-        "You subscribe to Gpt-4o on johnmobley99 account.",
-        "Switch to johnmobley99 account and add last conversation, to-do list for text2game, to this to-do list."
+        "You subscribe to GPT-4 on johnmobley99 account.",
+        "You subscribe to GeminiAdvanced on alexander.mobley account.",
+        "You subscribe to Claude Opus on jmobleyworks account.",
+        "Consider Copilot Pro.",
+        "Switch to johnmobley99 account and add the last conversation and to-do list for text2game to this to-do list."
     ],
     "Meranda": [
         "Write an epic Villanelle called 'Oh To Be Young With You' to Meranda."
     ],
     "ClaudeAI TAB": [
         "Deploy the DB invention."
-    ]
+    ],
     "Art Gen": [
-        "Use Todo: Art Gen in apple notes app to see more details"
+        "Use Todo: Art Gen in Apple Notes app to see more details."
+    ],
+    "Additional Tasks": [
+        "Put the requirements in the chatgpt tab into text2game.",
+        "Mess around with non-Euclidean game world.",
+        "Copy junk from RedBlockGames.",
+        "Output sonnets, terza rima, and villanelles."
     ]
-    put the reqs in chatgpt tab into text2game
-    mess around with non Euclidian game World
-    copy junk from RedBlockGames
-    output sonnets terzarima and villanelles
 };
+
+//
+export const spriteAsJson = {
+    
+    "0": ["Metadata","Name:SpriteName","Size:SpriteSize","Description:SpriteDescription","GeneratingPrompt:SpriteGeneratingPrompt"],
+    "1": ["x:0","y:0","r:0", "g:0", "b:0", "a:0"],
+    "2": ["x:1","y:0","r:0", "g:0", "b:0", "a:0"],
+    "3": ["x:0","y:1","r:0", "g:0", "b:0", "a:0"],
+    "4": ["x:1","y:1","r:0", "g:0", "b:0", "a:0"],
+    };

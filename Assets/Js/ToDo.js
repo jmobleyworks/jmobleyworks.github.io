a8bc7253389194321a414b3910d82943c7353673
jmobleyworks <77175845+jmobleyworks@users.noreply.github.com>
2024-05-30 16:42:13 -0400

    gg

---
+++ Assets/Js/ToDo.js
@@ -0,0 +1,52 @@
+export const ToDo = {
+    "2dActionRpg": [
+        "Remove the map stretching thing and test that things work.",
+        "Write a function to export the map to an image to upload to AI art generator to turn white traversable and black non-traversable pixel art."
+    ],
+    "WorldSeed": [
+        "Make it so the prompt updates whenever a control is selected or the form is cleared.",
+        "Add copy and donate buttons.",
+        "Turn randomize and clear buttons into glyphs and put all four at the top right of World Builder control."
+    ],
+    "WorldBender": [
+        "Make the structure appear on load and provide an explanation.",
+        "Put it in the style of the Mobleysoft Brand.",
+        "Come up with a name for my tool whose research paper will be something like 'A sufficiently advanced game world model as reality' that expresses how the research into perfecting a gameWorld model can be extrapolated to designing a powerful World Model, an AI that demonstrates an understanding of the world."
+    ],
+    "SpriteDetector": [
+        "Put it in the style of the Mobleysoft Brand.",
+        "Add sprite sheets generated so far as buttons and let them be clicked to switch what sprite sheet has its sprites extracted."
+    ],
+    "chatGpT": [
+        "You subscribe to GPT-4 on johnmobley99 account.",
+        "You subscribe to GeminiAdvanced on alexander.mobley account.",
+        "You subscribe to Claude Opus on jmobleyworks account.",
+        "Consider Copilot Pro.",
+        "Switch to johnmobley99 account and add the last conversation and to-do list for text2game to this to-do list."
+    ],
+    "Meranda": [
+        "Write an epic Villanelle called 'Oh To Be Young With You' to Meranda."
+    ],
+    "ClaudeAI TAB": [
+        "Deploy the DB invention."
+    ],
+    "Art Gen": [
+        "Use Todo: Art Gen in Apple Notes app to see more details."
+    ],
+    "Additional Tasks": [
+        "Put the requirements in the chatgpt tab into text2game.",
+        "Mess around with non-Euclidean game world.",
+        "Copy junk from RedBlockGames.",
+        "Output sonnets, terza rima, and villanelles."
+    ]
+};
+
+//
+export const spriteAsJson = {
+    
+    "0": ["Metadata","Name:SpriteName","Size:SpriteSize","Description:SpriteDescription","GeneratingPrompt:SpriteGeneratingPrompt"],
+    "1": ["x:0","y:0","r:0", "g:0", "b:0", "a:0"],
+    "2": ["x:1","y:0","r:0", "g:0", "b:0", "a:0"],
+    "3": ["x:0","y:1","r:0", "g:0", "b:0", "a:0"],
+    "4": ["x:1","y:1","r:0", "g:0", "b:0", "a:0"],
+    };
